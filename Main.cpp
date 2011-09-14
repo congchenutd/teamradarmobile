1ae2ea53e203ad1369f5a1330c47f4199e57ba2c
congchenutd <congchenutd@f0b8e93a-80c3-2f67-6146-92bfbd0f8b4f>
2011-09-14 20:56:10 +0000

    = WelcomeDlg launched from main()

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,30 @@
 #include "MainWindow.h"
+#include "WelcomeDlg.h"
 #include <QtGui/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+	// font
 	QFont font = app.font();
 	font.setPointSize(10);
 	app.setFont(font);
 
+	// user clicked quit
+	WelcomeDlg welcome;
+	if(welcome.exec() != QDialog::Accepted)
+		return 1;
+
+	// main window
     MainWindow mainWindow;
 	mainWindow.setOrientation(MainWindow::ScreenOrientationAuto);
 	mainWindow.showMaximized();
+
+	if(welcome.isOnline())
+		mainWindow.onOnline();
+	else
+		mainWindow.onOffline();
 
     return app.exec();
 }
